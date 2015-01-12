ad998f97ebd744dd24d8f80fbd891219b9b18970
enrique7mc <enrique7mc@gmail.com>
2015-01-11 21:43:39 -0600

    Añade el código para leer datos del teclado

--- ProyectoCSharpIntermedio/Program.cs
+++ ProyectoCSharpIntermedio/Program.cs
@@ -8,8 +8,30 @@ namespace ProyectoCSharpIntermedio
 {
     class Program
     {
+        private static readonly ExporadorDeArchivos Exporador = 
+            new ExporadorDeArchivos(@"C:\Users\Enrique\Documents");
+
         static void Main(string[] args)
         {
+            string entrada;
+            while(true)
+            {
+                try
+                {
+                    entrada = Promt();
+                    Exporador.EjecutaComando(InterpreteDeComandos.Interpretar(entrada));
+                }
+                catch(ComandoInvalidoException e)
+                {
+                    Console.WriteLine(e.Message);                    
+                }
+            }
+        }
+
+        static string Promt()
+        {
+            Console.Write(Exporador.DirectorioActual + "> ");
+            return Console.ReadLine();
         }
     }
 }
