bc6015c475b77f650672b1c288740156d5a86039
enrique7mc <enrique7mc@gmail.com>
2015-01-11 19:52:50 -0600

    Añade proyecto de pruebas

---
+++ ProyectoTests/InterpreteTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoCSharpIntermedio;
+
+namespace ProyectoTests
+{
+    [TestClass]
+    public class InterpreteTests
+    {
+
+        [TestMethod]
+        public void PruebaInterprete()
+        {
+            string[] cadenas =
+            {
+                "dir", "cd directorio", "copy archivo otro archivo",
+                "", "   ", "touch archivo"
+            };
+
+            List<Comando> resultados = new List<Comando>();
+
+            foreach(var cadena in cadenas)
+            {
+                resultados.Add(InterpreteDeComandos.Interpretar(cadena));
+            }
+
+            Assert.AreEqual(4, resultados.Count);
+        }
+    }
+}
