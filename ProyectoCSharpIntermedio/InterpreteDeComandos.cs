65b4c28f91c67fd292eb8c6877cae2651cb5b861
enrique7mc <enrique7mc@gmail.com>
2015-01-11 19:54:46 -0600

    Añade la clase InterpreteDeComandos
    Toma una cadena de entrada y revuelve el comando asociado a ella

---
+++ ProyectoCSharpIntermedio/InterpreteDeComandos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCSharpIntermedio
+{
+    public class InterpreteDeComandos
+    {
+        public static Comando Interpretar(string entrada)
+        {
+            var tokens = Split(entrada);
+            if(tokens.Length == 0)
+            {
+                throw new ComandoInvalidoException("Comando no válido");
+            }
+
+            return new Comando(tokens[0], tokens.Skip(1).ToArray());
+        }
+
+        static string[] Split(string cadena)
+        {
+            return cadena.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
