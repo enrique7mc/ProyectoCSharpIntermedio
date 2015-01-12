acd06649a39f9ea56007cd0eb1f70cc3bc3eb45a
enrique7mc <enrique7mc@gmail.com>
2015-01-11 19:54:13 -0600

    Añade la clase ComandoInvalidoException
    Lanzada cuando fallo la interpretación del comando

---
+++ ProyectoCSharpIntermedio/ComandoInvalidoException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProyectoCSharpIntermedio
+{
+    public class ComandoInvalidoException : Exception
+    {
+        public ComandoInvalidoException()
+        {
+        }
+
+        public ComandoInvalidoException(string message) : base(message)
+        {
+        }
+
+        public ComandoInvalidoException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
